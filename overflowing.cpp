52c90e4b52be4e8f5664ae248a82d4c0347cddd0
Flareragnarok <flareragnarok99@gmail.com>
2019-10-02 17:32:15 +0200

    adding two more
    its practice man, what do i need to exbrain here

---
+++ overflowing.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<limits>
+using namespace std;
+
+int main(){
+	int ai {50000}; //initialization with 50k
+	int bi {10000000}; //initialization with 1m
+	int ci {ai*bi}; //init with produt of ai and bi
+	
+	cout<< "int numbers have: "<< 8*sizeof(int) << " Bits on your system\n";
+	cout<< "here calculation with int: ";
+	cout<< ai<< " * " << bi << " = "<< ci << '\n';
+	cout<< "instead of 50 000 000 000, you got"<< ci << '\n';
+	
+	
+	long al {50000L}; //init using L/l for long
+	long bl {10000000L}; 
+	long cl {al*bl}; //init with produt of al and bl
+	
+	cout<< "long numbers have: "<< 8*sizeof(long) << " Bits on your system\n";
+	cout<< "here calculation with long: ";
+	cout<< al<< " * " << bl << " = "<< cl << '\n';
+	cout<< "instead of 50 000 000 000, you got "<< cl;
+	al = (numeric_limits<long>::max()/2)+1000;
+	cout << "2 * "<< al << " = " << (2*al) << "?\n";
+	int f = 3;
+	int g = f;
+	int h;
+	f = f^3;
+	h = g>>2;
+	cout << ~f <<" "<< -g <<" "<< h <<'\n';
+	
+	
+	
+	return 0;
+}
---
+++ practice1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<limits>
+#include<cstdint>
+using namespace std;
+
+int main(){
+size_t f = 2;
+
+cout << f;
+}
